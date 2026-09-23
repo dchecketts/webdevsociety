f4c145edf35c282b89a12505feab8da0d0fbbfa6
Daniel Checketts <danielkchecketts@gmail.com>
2026-09-22 19:41:08 -0600

    Updated routing

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-  site: 'https://dchecketts.github.io',
-  base: '/society-site-refactor',
+  site: 'https://web-design-development-society.github.io/',
+  base: '/webdevsociety',
   output: 'static'
 });
